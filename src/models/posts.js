43883edda1ea82cf6d55ee0927ad14bea7603039
Ashwin Kumar S <ashwinramuk@gmail.com>
2022-12-29 00:30:18 +0530

    Schema field name change

--- src/models/posts.js
+++ src/models/posts.js
@@ -5,10 +5,10 @@ const ObjectId = Schema.Types.ObjectId
 const postSchema = new Schema({
     title:{type:String, required:true},
     content:{type:String, required:true},
-    imgURL:{type:String, required:true},
+    postImage:{type:String, required:true},
     author:{type:String},
-    dateCreated:{type:Date, default:Date.now},
-    userID:{type:ObjectId ,ref:"users"}
+    postCreatedAt:{type:Date, default:Date.now},
+    user:{type:ObjectId ,ref:"users"}
 })
 
 const posts = mongoose.model("posts", postSchema)
